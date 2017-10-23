88d5cb0961918c830ee5e0a3ff66987ad83b7c15
yn <nevalenny@gmail.com>
2017-10-23 17:33:15 +0300

    Fix mono version

--- mobile-center-pre-build.sh
+++ mobile-center-pre-build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+MONOPREFIX=/Library/Frameworks/Mono.framework/Versions/5_4_0
+echo "##vso[task.setvariable variable=DYLD_FALLBACK_LIBRARY_PATH;]$MONOPREFIX/lib:/lib:/usr/lib:$DYLD_LIBRARY_FALLBACK_PATH"
+echo "##vso[task.setvariable variable=PKG_CONFIG_PATH;]$MONOPREFIX/lib/pkgconfig:$MONOPREFIX/share/pkgconfig:$PKG_CONFIG_PATH"
+echo "##vso[task.setvariable variable=PATH;]$MONOPREFIX/bin:$PATH"
+
 $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh 5_4_0
